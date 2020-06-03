b2f122375e150c79d252fd9acb417c87e5f8efd3
Ashtonn77 <ashtonrbay@gmail.com>
2020-06-03 22:13:06 +0200

    added javscript functionality

--- index.js
+++ index.js
@@ -3,3 +3,43 @@ const play = document.getElementById("play");
 const stop = document.getElementById("stop");
 const timestamp = document.getElementById("timestamp");
 const progress = document.getElementById("progress");
+
+//functions
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+function updatePlayIcon() {
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause"></i>';
+  }
+}
+
+function updateProgress() {
+  return true;
+}
+
+function stopVideo() {
+  video.currentTime = 0;
+  video.pause();
+}
+
+function setVideoProgress() {
+  return true;
+}
+
+//event listeners
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("timeupdate", updateProgress);
+
+play.addEventListener("click", toggleVideoStatus);
+stop.addEventListener("click", stopVideo);
+progress.addEventListener("change", setVideoProgress);
